1d1bd2b72602e135ca803b3781cfd8f08d2a478b
Rven721 <amalyavin1@gmail.com>
2021-10-10 08:53:50 +0300

    some template commits

--- conf/settings.py
+++ conf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DATE_INPUT_FORMATS = '%d/%m/%Y'
 
@@ -128,3 +130,5 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'main'
 LOGOUT_URL = 'logout'
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
--- crm/my_test.py
+++ crm/my_test.py
@@ -1,19 +1,7 @@
 import os
-# import django
-# import math
-#
-#
-# # def toFixed(numObj, digits=0):
-# #     return f"{numObj:.{digits}f}"
-#
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'conf.settings'
-# django.setup()
-# from crm.models import Company, Project
-#
-# companies = Company.objects.all()
-# for company in companies:
-#     print(company.short_name)
-# projects = Project.objects.all()
-# print(projects.filter(company=companies[4]))
 
-os.environ['SECRET_KEY'] = '=h^(in!mt*^=lkr&@@u3bk8132ux+j@7*#33%92y^+4&)67ws!'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
+print(os.path.dirname(os.path.abspath('admin.py')))
+print(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath('admin.py')))))
