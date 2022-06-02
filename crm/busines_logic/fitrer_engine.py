c48a7c7687ded3b8d5c295b5213c30f493c296f5
Rven721 <amalyavin1@gmail.com>
2022-06-02 09:22:41 +0300

    incomplete events highlighting

--- crm/busines_logic/fitrer_engine.py
+++ crm/busines_logic/fitrer_engine.py
@@ -9,6 +9,7 @@ from crm.models import Project
 
 
 def project_list_filter(company=None, project_deliver=None, project_status=None):
+    """will return a list of projects based on given data"""
     result = Project.objects.order_by('name')
     if project_deliver:
         result = result.filter(project_deliver=project_deliver)
@@ -24,3 +25,14 @@ def project_list_filter(company=None, project_deliver=None, project_status=None)
             except AttributeError:
                 result.remove(project)
     return result
+
+
+def get_incomplete_events(events):
+    """Will return a events with incomplete tasks"""
+    incomplete_events = []
+    for event in events:
+        for task in event.tasks.all():
+            if task.statuses.all().last().status != 'DONE':
+                incomplete_events.append(event)
+                break
+    return incomplete_events
