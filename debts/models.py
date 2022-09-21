e59b77609db06386752ff669aa556dea2e08d1d0
Rven721 <amalyavin1@gmail.com>
2022-09-21 10:30:06 +0300

    try to fix Person model

--- debts/models.py
+++ debts/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Person(models.Model):
     """A class for person who can bee a creditr or a debtor"""
     name = models.CharField(max_length=30, verbose_name="Имя", unique=True)
-    name_in_dat = models.CharField(max_length=30, default='_dat', verbose_name="Имя в дательном падеже")
+    name_in_dat = models.CharField(max_length=150, default='dat', verbose_name="Имя в дательном падеже")
 
     def __str__(self):
         return self.name
