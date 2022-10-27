14a1f69d5fcdc7cdb57cedb953b1619f54aeb7d0
Rven721 <amalyavin1@gmail.com>
2022-10-27 16:06:32 +0300

    undone task enouncment added

--- crm/templatetags/undone_task_checker.py
+++ crm/templatetags/undone_task_checker.py
@@ -1,6 +1,6 @@
 """Filter for to check wether project has undone tasks"""
 from django import template
-from ..models import Project
+from ..models import Project, Task
 
 register = template.Library()
 
@@ -14,4 +14,14 @@ def has_undone_tasks(project_id: int) -> bool:
         for task in event.tasks.all():
             if task.statuses.all().last().status != 'DONE':
                 return True
+    return False
+
+
+@register.filter
+def user_has_undone_tasks(user_id: int) -> bool:
+    """Will return True if user has undone tasks"""
+    tasks = Task.objects.filter(doer__id=user_id)
+    for task in tasks:
+        if task.statuses.last().status in ['NEW', 'PROGRESS']:
+            return True
     return False
