67dcbf94fc8364444fdb6e16e31399a486ffab54
Rven721 <amalyavin1@gmail.com>
2022-12-18 08:26:49 +0300

    global database added

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -85,8 +85,10 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_db',
+        'USER': 'alex',
+        'PASSWOR': os.environ.get('DB_PASSWORD'),
     },
 }
 
@@ -121,7 +123,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+#USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
--- set_env.sh
+++ set_env.sh
@@ -1,1 +1,2 @@
 export DJANGO_SETTINGS_MODULE=conf.settings.base
+export DB_PASSWORD=789122 
